66e8c61466dd7ed2c9be89680966897a3b811431
hackerdlk <115404040+hackerdlk@users.noreply.github.com>
2026-01-18 14:44:59 +0800

    Max0118C

---
+++ assets/processImagePaths-BqwZ3khn.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-B3lxVx77.js";function o(a){return{...a,coverImage:a.coverImage?e(a.coverImage):void 0}}function t(a){return{...a,avatar:a.avatar?e(a.avatar):void 0}}export{t as a,o as p};
